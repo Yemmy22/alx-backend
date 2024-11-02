3d352934481dcabcec1b94490d772f800e7b95ad
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-02 02:35:19 +0000

    Implements a method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+A module for simple pagination.
+"""
+
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Caches dataset if not loaded and returns the dataset.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Exclude header
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns the appropriate page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        return dataset[
+                start_index:end_index
+                ] if start_index < len(dataset) else []
