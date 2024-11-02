5467a2e4462ce60c0fa1e3b9df23b7a76adf1a9c
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-02 01:33:20 +0000

    Fix

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -31,9 +31,8 @@ class MRUCache(BaseCaching):
         if key in self.cache_data:
             self.cache_data.pop(key)
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            most_recent_key = next(reversed(self.cache_data))
-            self.cache_data.pop(most_recent_key)
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            most_recent_key, _ = self.cache_data.popitem(last=True)
             print(f"DISCARD: {most_recent_key}")
 
         self.cache_data[key] = item
